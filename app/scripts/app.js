cd1f61e1e5697ef924694c915b217e561abc2e11
yqu1 <quyaoxian@gmail.com>
2017-01-14 01:47:26 -0500

    dueling-system

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fencingApp', ['ui.router', 'ngDialog', 'ngResource', 'ui.materialize'])
+angular.module('fencingApp', ['ui.router', 'ngDialog', 'ngResource', 'ui.materialize', 'ngSanitize'])
 .config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider
 
@@ -8,7 +8,8 @@ angular.module('fencingApp', ['ui.router', 'ngDialog', 'ngResource', 'ui.materia
 			url: '/',
 			views: {
 				'header': {
-					templateUrl: 'views/mainheader.html'
+					templateUrl: 'views/mainheader.html',
+					controller: 'fencingController'
 				},
 				'content': {
 					templateUrl: 'views/main.html',
@@ -16,6 +17,34 @@ angular.module('fencingApp', ['ui.router', 'ngDialog', 'ngResource', 'ui.materia
 				},
 				'footer': {
 					templateUrl: 'views/footer.html'
+				}
+			}
+		})
+
+		.state('app.schedule', {
+			url: 'schedule',
+			views: {
+				'content@': {
+					templateUrl: 'views/schedule.html',
+				}
+			}
+		})
+
+		.state('app.login', {
+			url: 'login',
+			views: {
+				'content@': {
+					templateUrl: 'views/registration/login.html',
+					controller: 'loginController'
+				}
+			}
+		})
+
+		.state('app.resources', {
+			url: 'resources',
+			views: {
+				'content@': {
+					templateUrl: 'views/resources.html',
 				}
 			}
 		})
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,5 +1,47 @@
 'use strict'
+
 angular.module('fencingApp')
-.controller('fencingController', function($scope) {
-  
-});
+.service('blogService', ['$http', function($http) {
+	var url = 'https://www.googleapis.com/blogger/v3/blogs/7067178439209854231/posts?key=AIzaSyClkD7VwG7pUmxVqDqBcTUZ5PzjWbSHGXA'
+	this.getNewBlog = function(callback) {
+		$http.get(url).then(function(response) {
+			callback(response.data)
+		})
+	}
+}])
+
+
+.controller('fencingController', ['$scope', 'blogService', '$sce', function($scope, blogService, $sce) {
+  	blogService.getNewBlog(function(data) {
+  		$scope.post1 = {
+  			newPost: data.items[0]["content"],
+	  		newPost_url: data.items[0]["url"],
+	  		title:  data.items[0]["title"],
+	  		author:  data.items[0]["author"]["displayName"],
+	  		author_url:  data.items[0]["author"]["url"],
+	  		newDate:  new Date(data.items[0]["published"])
+  		}
+
+  		$scope.post2 = {
+  			newPost: data.items[1]["content"],
+	  		newPost_url: data.items[1]["url"],
+	  		title:  data.items[1]["title"],
+	  		author:  data.items[1]["author"]["displayName"],
+	  		author_url:  data.items[1]["author"]["url"],
+	  		newDate:  new Date(data.items[1]["published"])
+  		}
+
+      $scope.post3 = {
+        newPost: data.items[2]["content"],
+        newPost_url: data.items[2]["url"],
+        title:  data.items[2]["title"],
+        author:  data.items[2]["author"]["displayName"],
+        author_url:  data.items[2]["author"]["url"],
+        newDate:  new Date(data.items[2]["published"])
+      }
+  	})
+  	$scope.scrollBottom = function() {
+  		window.scrollTo(0,document.body.scrollHeight);
+  	}
+}])
+
